b70fa9068239e301ab6380bfaf1b5051b5dfb995
philips-of-ng@philips <philipsedun@gmail.com>
2025-05-07 13:58:20 +0100

    updated

--- src/components/PHANav.jsx
+++ src/components/PHANav.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../css/pha-nav.css'
 import { AuthContext } from '../context/AuthContext';
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
-
+import { useLocation } from 'react-router-dom';
 // THIS IS THE GENRAL NAV FOR HISTORY, ACTIVITIES AND PROFILE PAGES
 
 const PHANav = () => {
+
+  const currentPage = useLocation().pathname
+  const activePHA = useLocation().pathname.replaceAll('/', '')
+
+  useEffect(() => {
+    console.log('The current page is', currentPage);
+  }, [])
 
   const navigate = useNavigate()
   const { user, logout } = useContext(AuthContext);
@@ -17,21 +24,21 @@ const PHANav = () => {
         <img src={user.displayPicture} alt="" />
 
         <div>
-          <p>My Activity</p>
+          <p>{currentPage.includes('history') ? 'History' : currentPage.includes('settings') ? 'Settings' : currentPage.includes('profile') ? 'Profile' : 'Unknown'}</p>
         </div>
       </div>
 
       <div className='right'>
-        <button>
-          <i className='bx bx-scan'></i>
+        <button className={`${activePHA === 'profile' ? 'active-pha' : ''}`} onClick={() => navigate('/profile-page')}>
+          <i className='bx bx-user'></i>
         </button>
 
-        <button onClick={() => navigate('/history')}>
-          <i className='bx bx-time'></i>
+        <button className={`${activePHA === 'history' ? 'active-pha' : ''}`} onClick={() => navigate('/history')}>
+          <i className={`bx bx-time`}></i>
         </button>
 
-        <button onClick={() => navigate('/profile/settings')}>
-          <i className='bx bx-cog' ></i>
+        <button className={`${activePHA === 'settings' ? 'active-pha' : ''}`} onClick={() => navigate('/profile/settings')}>
+          <i className={`bx bx-cog`}></i>
         </button>
       </div>
     </div>
